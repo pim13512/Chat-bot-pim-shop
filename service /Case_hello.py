9e5442a6c7d12431addacec68ba65e5617e20cae
pim13512 <114812105+pim13512@users.noreply.github.com>
2023-05-14 22:24:30 +0700

    Create Case_hello.py

---
+++ service /Case_hello.py
@@ -0,0 +1,15 @@
+def hello_function():
+    return {
+        "fulfillmentMessages": [
+            {
+                "image": {
+                    "imageUri": "https://cms.dmpcdn.com/travel/2021/10/14/53962cd0-2ce0-11ec-be9d-838428bcfc5b_original.jpg",
+                }
+            },
+            {
+                "image": {
+                    "imageUri": "https://cms.dmpcdn.com/travel/2021/10/14/53962cd0-2ce0-11ec-be9d-838428bcfc5b_original.jpg",
+                }
+            }
+        ]
+    }
